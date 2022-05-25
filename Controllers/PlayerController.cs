024b304873059d9c712faa287627b781e2db1e30
caemanswartz <caemanswartz@gmail.com>
2022-05-24 18:53:41 -0700

    added basic authorization to player and bot controllers

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using QuintrixHomeworkPlayerMVP.Data;
 using QuintrixHomeworkPlayerMVP.Models;
 
 namespace QuintrixHomeworkPlayerMVP.Controllers
 {
+    [Authorize]
     public class PlayerController : Controller
     {
         private readonly ApplicationDbContext _context;
