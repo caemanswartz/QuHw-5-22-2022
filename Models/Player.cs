cfa17cb4d7ad97e8272b5b23d48cfdfde50b7fa0
caemanswartz <caemanswartz@gmail.com>
2022-05-26 12:27:57 -0700

    added cookies

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Identity;
 namespace QuintrixHomeworkPlayerMVP.Models
 {
     public class Player : IdentityUser
     {
+        [PersonalData]
         public string Name{get;set;}
+        [PersonalData]
         public uint Currency{get;set;}
         #pragma warning disable CS8618
         public Player(){}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,29 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using QuintrixHomeworkPlayerMVP.Data;
 using QuintrixHomeworkPlayerMVP.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<Player>(
+builder.Services.AddIdentity<Player,IdentityRole>(
     options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme);
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
