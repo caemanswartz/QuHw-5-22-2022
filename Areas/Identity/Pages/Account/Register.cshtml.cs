df192868c43b803f5d6350263f4380692fc871f4
caemanswartz <caemanswartz@gmail.com>
2022-05-26 11:15:45 -0700

    cleaned up player and required name to register

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,10 @@ namespace QuintrixHomeworkPlayerMVP.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name="Full name")]
+            public string Name{get;set;}
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
--- Models/Player.cs
+++ Models/Player.cs
@@ -5,29 +5,10 @@ namespace QuintrixHomeworkPlayerMVP.Models
 {
     public class Player : IdentityUser
     {
-        private static Regex _validEmailPattern=new(
-            @"^[a-zA-Z0-9_-]+@(\w+\.)+\w{2,}$",
-            RegexOptions.Compiled
-        );
-        public new Guid Id{get;set;}
         public string Name{get;set;}
-        private string _email;
-        #pragma warning disable CS8618
-        public override string Email
-        #pragma warning restore CS8618
-        {
-            get=>_email;
-            set=>_email=ValidatedEmail(value);
-        }
         public uint Currency{get;set;}
         #pragma warning disable CS8618
         public Player(){}
         #pragma warning restore CS8618
-        private static string ValidatedEmail(string input)
-        {
-            if(! _validEmailPattern.IsMatch(input))
-                throw new Exception("Email pattern invalid");
-            return input;
-        }
     }
 }
